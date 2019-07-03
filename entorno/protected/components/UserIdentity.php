19d49c175d2d7a8dba972c67b88f635efb8968d8
WalterBrake <d_mob.underworld@hotmail.com>
2019-07-03 18:03:14 -0500

    db conection
    db conection and method to login user in userIdentity.php at
    authenticate()

--- entorno/protected/components/UserIdentity.php
+++ entorno/protected/components/UserIdentity.php
@@ -1,33 +1,34 @@
 <?php
-
 /**
- * UserIdentity represents the data needed to identity a user.
- * It contains the authentication method that checks if the provided
- * data can identity the user.
- */
+* UserIdentity represents the data needed to identity a user.
+* It contains the authentication method that checks if the provided
+* data can identity the user.
+*/
 class UserIdentity extends CUserIdentity
 {
+	private $_id
 	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
+	* Authenticates a user.
+	* The example implementation makes sure if the username and password
+	* are both 'demo'.
+	* In practical applications, this should be changed to authenticate
+	* against some persistent user identity storage (e.g. database).
+	* @return boolean whether authentication succeeds.
+	*/
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
+		$users=Users::model()->find("LOWER(username)=?",array(strtolower($this->username)));
+		if(!isset($users)&&$users===NULL)
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
+		elseif($users->password!==$this->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
 		else
-			$this->errorCode=self::ERROR_NONE;
+			{
+				$this->_id=$users->id;
+				$this->setState("email", $user->email);
+				$this->errorCode=self::ERROR_NONE;
+
+			}
 		return !$this->errorCode;
 	}
 }

2dc0f6ef18690cc7d5587eaaaef2e68e8f4e2b1c
WalterBrake <d_mob.underworld@hotmail.com>
2019-07-03 21:45:35 -0500

    menu clients
    hide or show clients

--- entorno/themes/classic/views/layouts/main.php
+++ entorno/themes/classic/views/layouts/main.php
@@ -32,9 +32,9 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
+				//array('label'=>'Home', 'url'=>array('/site/index')),
 				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Clients', 'url'=>array('/clients')),
+				array('label'=>'Clients', 'url'=>array('/clients'),'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
@@ -51,9 +51,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		
 	</div><!-- footer -->
 
 </div><!-- page -->
